eed4f86765f754744bdf80abbd3b2858dbe11d42
Dean Wyns <dean.wyns.t9335@student.hogent.be>
2014-10-30 17:37:17 +0100

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name joetzApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the joetzApp
+ */
+angular.module('joetzApp')
+  .controller('MainCtrl', function ($scope) {
+    console.log($scope);
+  });
